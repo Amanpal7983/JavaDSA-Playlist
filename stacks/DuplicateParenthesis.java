36f24b2f6efd92e98c9a61d490b7ac0e1814048d
Aman pal <amanpal7983@gmail.com>
2026-07-20 12:22:52 +0530

    add new files

---
+++ stacks/DuplicateParenthesis.java
@@ -0,0 +1,36 @@
+package stacks;
+import java.util.Stack;
+
+public class DuplicateParenthesis {
+    static Boolean isDuplicate(String s) {
+        Stack<Character> st = new Stack<>();
+
+        for(int i=0;i<s.length();i++){
+            char ch = s.charAt(i);
+
+            if(ch == ')'){
+                int count =0;
+                while(st.peek() !='('){
+                    count++;
+                    st.pop();
+                }
+                if(count<1){
+                    return true;
+                }
+                else{
+                    st.pop();
+                }
+            }
+            else{
+                st.push(ch);
+            }
+        }
+
+        return false;
+        
+    }
+    public static void main(String[] args) {
+        String s = "(a+b)";
+        System.out.println(isDuplicate(s));
+    }
+}
