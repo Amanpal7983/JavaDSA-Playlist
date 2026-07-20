b439d962d1fc7b1bb51587c33174a06645e80458
Aman pal <amanpal7983@gmail.com>
2026-07-20 11:24:28 +0530

    add new files

---
+++ stacks/ValidParenethesis.java
@@ -0,0 +1,41 @@
+package stacks;
+
+import java.util.Stack;
+
+public class ValidParenethesis {
+
+    static Boolean valid(String s) {
+
+        Stack<Character> st = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if (ch == '(' || ch == '[' || ch == '{') {
+                st.push(ch);
+            } else {
+                if (st.isEmpty()) {
+                    return false;
+                }
+                if ((st.peek() == '(' && ch == ')') || (st.peek() == '{' && ch == '}') || (st.peek() == '[' && ch == ']')) {
+                    st.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        if (st.isEmpty()) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        String s = "([{([])}])";
+
+        System.out.println(valid(s));
+    }
+}
