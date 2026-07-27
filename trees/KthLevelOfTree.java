62b6ec9bc1be28ab6ba703aa69b94cba8f873e77
Aman pal <amanpal7983@gmail.com>
2026-07-27 10:22:18 +0530

    add files

---
+++ trees/KthLevelOfTree.java
@@ -0,0 +1,36 @@
+package trees;
+
+public class KthLevelOfTree {
+
+  static void kLevel(Node root , int level,int k){
+    if(root ==null){
+      return;
+    }
+
+    if(level == k){
+      System.out.print(root.data + " ");
+      return;
+    }
+
+    kLevel(root.left, level+1, k);
+    kLevel(root.right, level+1, k);
+
+  }
+  public static void main(String[] args) {
+     Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(10);
+        root.right.left = new Node(20);
+        root.right.right = new Node(5);
+        root.left.left.left = new Node(4);
+        root.left.left.right = new Node(7);
+
+        root.right.right.right = new Node(7);
+
+        int k = 3;
+        kLevel(root, 1, k);
+  }
+  
+}
